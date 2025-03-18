d4f04f0eb9c65695869fcde2e72c54b0e07ac39a
Rahul <rahulaggarwal1016@gmail.com>
2025-03-18 15:48:31 -0400

    docker changes

--- frontend/src/APIClients/BaseAPIClient.ts
+++ frontend/src/APIClients/BaseAPIClient.ts
@@ -7,63 +7,63 @@ import { DecodedJWT } from "../types/AuthTypes";
 import { setLocalStorageObjProperty } from "../utils/LocalStorageUtils";
 
 const baseAPIClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+	baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 // Python API uses snake_case, frontend uses camelCase
 // convert request and response data to/from snake_case and camelCase through axios interceptors
 baseAPIClient.interceptors.response.use((response: AxiosResponse) => {
-  if (
-    response.data &&
-    response.headers["content-type"] === "application/json"
-  ) {
-    response.data = camelizeKeys(response.data);
-  }
-  return response;
+	if (
+		response.data &&
+		response.headers["content-type"] === "application/json"
+	) {
+		response.data = camelizeKeys(response.data);
+	}
+	return response;
 });
 
 baseAPIClient.interceptors.request.use(async (config: AxiosRequestConfig) => {
-  const newConfig = { ...config };
+	const newConfig = { ...config };
 
-  // if access token in header has expired, do a refresh
-  const authHeaderParts = config.headers.Authorization?.split(" ");
-  if (
-    authHeaderParts &&
-    authHeaderParts.length >= 2 &&
-    authHeaderParts[0].toLowerCase() === "bearer"
-  ) {
-    const decodedToken = jwtDecode(authHeaderParts[1]) as DecodedJWT;
+	// if access token in header has expired, do a refresh
+	const authHeaderParts = config.headers.Authorization?.split(" ");
+	if (
+		authHeaderParts &&
+		authHeaderParts.length >= 2 &&
+		authHeaderParts[0].toLowerCase() === "bearer"
+	) {
+		const decodedToken = jwtDecode(authHeaderParts[1]) as DecodedJWT;
 
-    if (
-      decodedToken &&
-      (typeof decodedToken === "string" ||
-        decodedToken.exp <= Math.round(new Date().getTime() / 1000))
-    ) {
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_BACKEND_URL}/auth/refresh`,
-        {},
-        { withCredentials: true },
-      );
+		if (
+			decodedToken &&
+			(typeof decodedToken === "string" ||
+				decodedToken.exp <= Math.round(new Date().getTime() / 1000))
+		) {
+			const { data } = await axios.post(
+				`${process.env.REACT_APP_BACKEND_URL}/auth/refresh`,
+				{},
+				{ withCredentials: true },
+			);
 
-      const accessToken = data.accessToken || data.access_token;
-      setLocalStorageObjProperty(
-        AUTHENTICATED_USER_KEY,
-        "accessToken",
-        accessToken,
-      );
+			const accessToken = data.accessToken || data.access_token;
+			setLocalStorageObjProperty(
+				AUTHENTICATED_USER_KEY,
+				"accessToken",
+				accessToken,
+			);
 
-      newConfig.headers.Authorization = accessToken;
-    }
-  }
+			newConfig.headers.Authorization = accessToken;
+		}
+	}
 
-  if (config.params) {
-    newConfig.params = decamelizeKeys(config.params);
-  }
-  if (config.data && !(config.data instanceof FormData)) {
-    newConfig.data = decamelizeKeys(config.data);
-  }
+	if (config.params) {
+		newConfig.params = decamelizeKeys(config.params);
+	}
+	if (config.data && !(config.data instanceof FormData)) {
+		newConfig.data = decamelizeKeys(config.data);
+	}
 
-  return newConfig;
+	return newConfig;
 });
 
 export default baseAPIClient;
